222795838d01d2b229603dca43b1e05cf838941f
sakthi5856k-cell <sakthi5856k@gmail.com>
2026-07-07 08:17:13 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -21,7 +21,7 @@ from pydantic import BaseModel, Field, EmailStr, ConfigDict, field_validator
 from api.auth import router as auth_router
 from api.staff import router as staff_router
 from api.discord_auth import router as discord_router
-from api.me import router as me_router
+
 
 mongo_url = os.environ['MONGO_URL']
 client = AsyncIOMotorClient(mongo_url)
